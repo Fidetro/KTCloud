6ca8eec96a42863e3325153600bac2cfbcde435d
fidetro <zykzzzz@hotmail.com>
2018-12-18 09:30:59 +0800

    hidden Mac agent

--- KTPasteBoard-Mac/AppDelegate.swift
+++ KTPasteBoard-Mac/AppDelegate.swift
@@ -7,14 +7,47 @@
 //
 
 import Cocoa
+import CloudKit
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate,KeyboardObserverDelegate {
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
 
-
+    var customMenu = NSMenu()
+    let obsever = KeyboardObserver.shareInstance()
+    var copyCacheString : String = ""
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        statusItem.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+        NSApp.applicationIconImage = NSImage(named: "StatusBarButtonImage")
+        obsever.delegate = self
+
+    }
+    
+    func keyboardChange(withInputedString inputedString: String, flag: CGEventFlags) {
+        
+        if inputedString.lowercased() == "c",(flag.rawValue & CGEventFlags.maskSecondaryFn.rawValue) > 0x0 {
+            
+            if let string = NSPasteboard.general.string(forType: .string) {
+                if copyCacheString != string {
+                    Cloud.sync(string: string) { [weak self] (_, error) in
+                        self?.copyCacheString = string
+                        debugPrintLog(error)
+                    }
+                }
+            }
+        }else if inputedString.lowercased() == "t",(flag.rawValue & CGEventFlags.maskSecondaryFn.rawValue) > 0x0 {
+            Cloud.pull { (value) in
+                if let value = value as? String {
+                    NSPasteboard.general.clearContents()
+                    NSPasteboard.general.setString(value, forType: .string)
+                    NSPasteboard.general.string(forType: .string)
+                }
+            }
+        }
         
     }
+
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
--- KTPasteBoard-Mac/ViewController.swift
+++ KTPasteBoard-Mac/ViewController.swift
@@ -7,43 +7,15 @@
 //
 
 import Cocoa
-import CloudKit
-class ViewController: NSViewController,KeyboardObserverDelegate {
+class ViewController: NSViewController {
 
-    
-    let obsever = KeyboardObserver.shareInstance()
-    var copyCacheString : String = ""
-    
-    func keyboardChange(withInputedString inputedString: String, flag: CGEventFlags) {
-        
-        if inputedString.lowercased() == "c",(flag.rawValue & CGEventFlags.maskSecondaryFn.rawValue) > 0x0 {
-            
-            if let string = NSPasteboard.general.string(forType: .string) {
-                if copyCacheString != string {
-                    Cloud.sync(string: string) { [weak self] (_, error) in
-                        self?.copyCacheString = string
-                        debugPrintLog(error)
-                    }
-                }
-            }
-        }else if inputedString.lowercased() == "t",(flag.rawValue & CGEventFlags.maskSecondaryFn.rawValue) > 0x0 {
-            Cloud.pull { (value) in
-                if let value = value as? String {
-                    NSPasteboard.general.clearContents()
-                    NSPasteboard.general.setString(value, forType: .string)
-                    NSPasteboard.general.string(forType: .string)
-                }
-            }
-        }
-        
-    }
-   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        obsever.delegate = self
+
 
     }
+    
 
     override var representedObject: Any? {
         didSet {
